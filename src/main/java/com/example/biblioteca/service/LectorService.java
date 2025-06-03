331532e4149f04098d536940629b2eab19005c1e
Chema Fernandez <josemariafcanton@uma.es>
2025-06-03 12:02:43 +0200

    pendiente de revisar el metodo multar

--- src/main/java/com/example/biblioteca/service/LectorService.java
+++ src/main/java/com/example/biblioteca/service/LectorService.java
@@ -1,7 +1,7 @@
 package com.example.biblioteca.service;
 
-
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
@@ -22,113 +22,117 @@ import com.example.biblioteca.model.Multa;
 @Service
 public class LectorService {
 
-    private final LectorRepository repository;
-    private final CopiaRepository copiaRepository;
-    
-    @Autowired
-    private PrestamoRepository prestamoRepository;
-    
-    @Autowired
-    public LectorService(LectorRepository repository, CopiaRepository copiaRepository) {
-        this.repository = repository;
-        this.copiaRepository = copiaRepository;
-        this.prestamoRepository = prestamoRepository;
+	private final LectorRepository repository;
+	private final CopiaRepository copiaRepository;
 
-    }
-    
-    public List<Lector> getAllLectores() {
-        return repository.findAll();
-    }
+	@Autowired
+	private PrestamoRepository prestamoRepository;
 
-    public Optional<Lector> getLectorById(Long id) {
-        return repository.findById(id);
-    }
+	@Autowired
+	public LectorService(LectorRepository repository, CopiaRepository copiaRepository) {
+		this.repository = repository;
+		this.copiaRepository = copiaRepository;
+		this.prestamoRepository = prestamoRepository;
 
-    public Lector createLector(Lector lector) {
-        return repository.save(lector);
-    }
+	}
 
-    public Optional<Lector> updateLector(Long id, Lector lectorDetails) {
-        return repository.findById(id).map(lector -> {
-            lector.setNombre(lectorDetails.getNombre());
-            lector.setTelefono(lectorDetails.getTelefono());
-            lector.setDireccion(lectorDetails.getDireccion());
-            return repository.save(lector);
-        });
-    }
+	public List<Lector> getAllLectores() {
+		return repository.findAll();
+	}
 
-    public boolean deleteLector(Long id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public Optional<Lector> getLectorById(Long id) {
+		return repository.findById(id);
+	}
 
-    public boolean devolver(Long id, Copia copia) {
-        if (copia == null) {
-            throw new IllegalArgumentException("La copia no puede ser null");
-        }
+	public Lector createLector(Lector lector) {
+		return repository.save(lector);
+	}
 
-        Optional<Lector> lectorOpt = repository.findById(id);
-        if (lectorOpt.isPresent()) {
-            Lector lector = lectorOpt.get();
-            Optional<Prestamo> prestamoOpt = lector.getPrestamos().stream()
-                .filter(prestamo -> prestamo.getCopia().getId().equals(copia.getId())) // Actualització aquí
-                .findFirst();
+	public Optional<Lector> updateLector(Long id, Lector lectorDetails) {
+		return repository.findById(id).map(lector -> {
+			lector.setNombre(lectorDetails.getNombre());
+			lector.setTelefono(lectorDetails.getTelefono());
+			lector.setDireccion(lectorDetails.getDireccion());
+			return repository.save(lector);
+		});
+	}
 
-            if (prestamoOpt.isPresent()) {
-                Prestamo prestamo = prestamoOpt.get();
-                lector.getPrestamos().remove(prestamo);
-                copia.setEstado(EstadoCopia.BIBLIOTECA);
-                repository.save(lector);
-                copiaRepository.save(copia);
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean deleteLector(Long id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public boolean prestar(Long id, Libro libro) {
-        if (libro == null) {
-            throw new IllegalArgumentException("El libro no puede ser null");
-        }
+	public boolean devolver(Long id, Copia copia, LocalDate fechaDevolucion, Long prestamoId) {
+		if (copia == null) {
+			throw new IllegalArgumentException("La copia no puede ser null");
+		}
+		System.out.print("Lector" + id);
+		Optional<Lector> lectorOpt = repository.findById(id);
+		if (lectorOpt.isPresent()) {
+			Lector lector = lectorOpt.get();
+			Optional<Prestamo> prestamoOpt = lector.getPrestamos().stream()
+					.filter(prestamo -> prestamo.getCopia().getId().equals(copia.getId())) // Actualització aquí
+					.findFirst();
+			System.out.println("Entra primero");
+			if (prestamoOpt.isPresent()) {
+				System.out.println("Entra");
+				Prestamo prestamo = prestamoOpt.get();
+				lector.getPrestamos().remove(prestamo);
+				prestamo.setFin(Date.valueOf(LocalDate.now()));
+				copia.setEstado(EstadoCopia.BIBLIOTECA);
+				repository.save(lector);
+				copiaRepository.save(copia);
+				prestamoRepository.save(prestamo);
+		        long diferenciaDias = ChronoUnit.DAYS.between(prestamo.getFin(), prestamo.getInicio());
+		        multar(id, diferenciaDias);
+				return true;
+			}
+		}
+		return false;
+	}
 
-        Optional<Lector> lectorOpt = repository.findById(id);
-        if (lectorOpt.isPresent()) {
-            Lector lector = lectorOpt.get();
-            if (!lector.tieneMultaActiva() && lector.getPrestamos().size() < 3) {
-                Copia copia = libro.buscarCopiaDisponible();
-                if (copia != null) {
-                    Date fechaAct = Date.valueOf(LocalDate.now());
-                    Date fechaFin = new Date(fechaAct.getTime() + (30L * 24 * 60 * 60 * 1000));
-                    Prestamo prestamo = new Prestamo(fechaAct, fechaFin, copia, lector); // Actualització aquí
-                    copia.setEstado(EstadoCopia.PRESTADO);
-                    lector.getPrestamos().add(prestamo);
-                    repository.save(lector);
-                    copiaRepository.save(copia);
-                    prestamoRepository.save(prestamo);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	public boolean prestar(Long id, Libro libro) {
+		if (libro == null) {
+			throw new IllegalArgumentException("El libro no puede ser null");
+		}
 
-    public void multar(Long id, int diasRetraso) {
-        if (diasRetraso <= 0) {
-            throw new IllegalArgumentException("Los días de retraso deben ser mayores a 0");
-        }
+		Optional<Lector> lectorOpt = repository.findById(id);
+		if (lectorOpt.isPresent()) {
+			Lector lector = lectorOpt.get();
+			if (!lector.tieneMultaActiva() && lector.getPrestamos().size() < 3) {
+				Copia copia = libro.buscarCopiaDisponible();
+				if (copia != null) {
+					Date fechaAct = Date.valueOf(LocalDate.now());
+					Prestamo prestamo = new Prestamo(fechaAct, null, copia, lector); // Actualització aquí
+					copia.setEstado(EstadoCopia.PRESTADO);
+					lector.getPrestamos().add(prestamo);
+					repository.save(lector);
+					copiaRepository.save(copia);
+					prestamoRepository.save(prestamo);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-        Optional<Lector> lectorOpt = repository.findById(id);
-        if (lectorOpt.isPresent()) {
-            Lector lector = lectorOpt.get();
-            LocalDate inicio = LocalDate.now();
-            lector.setMulta(new Multa(inicio, inicio.plusDays(diasRetraso * 2)));
-            repository.save(lector);
-        } else {
-            throw new IllegalArgumentException("El lector con ID " + id + " no existe");
-        }
-    }
+	public void multar(Long id, int diasRetraso) {
+		if (diasRetraso <= 0) {
+			throw new IllegalArgumentException("Los días de retraso deben ser mayores a 0");
+		}
+
+		Optional<Lector> lectorOpt = repository.findById(id);
+		if (lectorOpt.isPresent()) {
+			Lector lector = lectorOpt.get();
+			LocalDate inicio = LocalDate.now();
+			lector.setMulta(new Multa(inicio, inicio.plusDays(diasRetraso * 2)));
+			repository.save(lector);
+		} else {
+			throw new IllegalArgumentException("El lector con ID " + id + " no existe");
+		}
+	}
 }
